86841ca77abda5c91f1723c4dd91cd8e2a8915ec
Peridot <peridot491@protonmail.com>
2023-06-05 15:31:25 +0200

    Inital commit

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    `java-library`
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+}
+
+allprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    group = "org.mythicprojects.unicornspigot"
+    version = "0.0.1-1.8.8.STABLE"
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven(url = "https://libraries.minecraft.net")
+        maven(url = "https://repo.titanvale.net/releases")
+        maven(url = "https://repo.titanvale.net/snapshots")
+    }
+
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(8)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
+        }
+    }
+}
